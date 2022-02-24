8fa8617d97391615ffb8e7437bb18e58a0d2e315
John S. Rajeski, Ed.D <48130054+rajeski@users.noreply.github.com>
2022-02-25 00:12:25 +0900

    Project Updates
    Continued coding this application.

--- weather.py
+++ weather.py
@@ -1,6 +1,10 @@
 # Import 
 from tkinter import *
 
+def search():
+    pass
+
+
 app = Tk()
 app.title('Weather App')
 app.geometry('700x350')
@@ -8,6 +12,11 @@ app.geometry('700x350')
 city_text = StringVar()
 city_entry = Entry(app, textvariable=city_text)
 city_entry.pack()
+
+search_btn =Button(app, text='Search Weather', width=12, command=search)
+search_btn.pack()
+
+location_lbl = Label(app, text='location', font=('bold', 20))
 
 
 app.mainloop()
