d2cf6245dfd39e4c5424ca6b3e9a0a908add098a
John S. Rajeski, Ed.D <48130054+rajeski@users.noreply.github.com>
2022-02-24 23:22:59 +0900

    Project Updates
    Continued adding Python code to build this application.

--- weather.py
+++ weather.py
@@ -4,3 +4,10 @@ from tkinter import *
 app = Tk()
 app.title('Weather App')
 app.geometry('700x350')
+
+city_text = StringVar()
+city_entry = Entry(app, textvariable=city_text)
+city_entry.pack()
+
+
+app.mainloop()
